7a07cf42af10018f93b8c1b558a6a4f69c6c97a8
Heutlett <adrian.araya@estudiantec.cr>
2022-09-01 02:01:40 -0600

    Add calc for vertical interpolation pixels in python algorithm

--- python_bilinear_interpolation/bilinear_interpolation.py
+++ python_bilinear_interpolation/bilinear_interpolation.py
@@ -10,26 +10,64 @@ def printer(array2d):
 
         print()
 
+#   Calcula el valor de pixel de interpolacion
+#   Parametros:     c1:     indice conocido1
+#                   c2:     indice conocido2
+#                   i:      indice numero a calcular
+#                   vc1:    valor del conocido1
+#                   vc2:    valor del conocido2
+def calc_interpolation(c1,c2,i,vc1,vc2):   
 
-def zeros_array(n):
+    return round(((c2-i)/(c2-c1))*vc1 + ((i-c1)/(c2-c1))*vc2)
 
-    a = []
+
+
+#   Calcula los pixeles centrales
+#   parametros:     n:      tamano de la fila
+#                   index:  indice del valor a calcular
+#                   I:      matriz
+#                   r:      fila
+def middle_pixels(n,index,I,r):
+
+    mid = []
 
     for i in range(n):
-        a.append(0)
+        
+        if (i%3==0):        # Pixeles verticales
 
-    return a
+            c1 = r*n+i+1                # indice conocido 1
+            c2 = (r+3)*n+i+1            # indice conocido 2
 
-def calc_horizontal(I,c,c1,c2,abi):
+            vc1 = I[r][i//3]            # valor del conocido1
+            vc2 = I[r+1][i//3]          # valor del conocido2
 
-    return round(((c2-abi)/(c2-c1))*I[r][c] + ((abi-c1)/(c2-c1))*I[r][c+1])
+            #print("c1: ",c1)
+            #print("c2: ",c2)
+            #print("index: ",index)
+            #print("vc1: ",vc1)
+            #print("vc2: ",vc2)
+
+            mid.append(calc_interpolation(c1,c2,index,vc1,vc2)) #c y g
+            
+            index = index +3
+        else:
+            mid.append(0)
 
 
-I = [[10,20],[30,40]]           #   Imagen inicial
+    return mid
 
-#I = [[10,20,50],[30,40,60],[30,40,60]]           #   Imagen inicial
+
+#   Matrices de prueba    
+
+#I = [[10,20],[30,40]]           #   Imagen inicial
+
+#I = [[10,20,30],[40,50,60],[70,80,90]]           #   Imagen inicial
 
 #I = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]           #   Imagen inicial
+
+#I = [[10,20,10,20],[30,40,30,40],[10,20,10,20],[30,40,30,40]]           #   Imagen inicial
+
+I = [[30,40],[10,20]]           #   Imagen inicial
 
 I_out = []                      #   Imagen interpolada
 for r in range(len(I)):
@@ -40,18 +78,21 @@ for r in range(len(I)):
 
         if(c < len(I[r])-1):
             
-            c1 = (c) + 1              # indice conocido 1
-            c2 = (c + 3) + 1          # indice conocido 2
-            ai = (c + 1) + 1         # indice de a
-            bi = (c + 2) + 1         # indice de a
+            c1 = (c) + 1                # indice conocido 1
+            c2 = (c + 3) + 1            # indice conocido 2
+            ai = (c + 1) + 1            # indice de a
+            bi = (c + 2) + 1            # indice de a
+            vc1 = I[r][c]               # valor del conocido1
+            vc2 = I[r][c+1]             # valor del conocido2
 
-            print("c1: ",c1)
-            print("c2: ",c2)
-            print("ai: ",ai)
+            #print("c1: ",c1)
+            #print("c2: ",c2)
+            #print("ai: ",ai)
+            #print("vc1: ",vc1)
+            #print("vc2: ",vc2)
 
-            I_new.append(calc_horizontal(I,c,c1,c2,ai))     #a
-
-            I_new.append(calc_horizontal(I,c,c1,c2,bi))     #b
+            I_new.append(calc_interpolation(c1,c2,ai,vc1,vc2))      #a
+            I_new.append(calc_interpolation(c1,c2,bi,vc1,vc2))      #b
             
     I_out.append(I_new)
 
@@ -59,8 +100,16 @@ for r in range(len(I)):
         
         n = 4 + (len(I[r])-2)*3
 
-        I_out.append(zeros_array(n))
-        I_out.append(zeros_array(n))
+        index1 = (r+1)*n+1
+        index2 = (r+2)*n+1
+
+        
+        print("index1:")
+        print(index1)
+        I_out.append(middle_pixels(n,index1,I,r))     #c
+        print("index2:")
+        print(index2)
+        I_out.append(middle_pixels(n,index2,I,r))     #g
 
 print("I:")
 printer(I)
